3282d0f1498e354c192abb703da1897e78454c22
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-24 17:59:01 +0200

    new_ListiniTableSeeder

---
+++ app/Listino.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listino extends Model
+{
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'listini';
+
+}
